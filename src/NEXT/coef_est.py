50965f53508b60b865364c0b7cad042d9e9eb63d
Daniel Philippus <dphilippus@mines.edu>
2025-02-03 13:50:08 -0700

    Update coef_est.py

--- src/NEXT/coef_est.py
+++ src/NEXT/coef_est.py
@@ -17,7 +17,7 @@ from NEWT import analysis, statics, Watershed
 inp_cols = ["tmax", "prcp", "srad", "vp",
             "area", "elev_min", "elev", "slope",
             "forest", "wetland", "developed", "ice_snow", "water",
-            "canopy", "flowdir",
+            "canopy", "flowdir", "ws_canopy",
             "lat", "lon", "date", "day"]
 req_cols = inp_cols + ["id"]
 training_req_cols = req_cols + ["temperature"]
